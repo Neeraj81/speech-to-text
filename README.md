bbb29219bff7b31e45af110191c702117f673c18
Neeraj81 <53859942+Neeraj81@users.noreply.github.com>
2021-08-03 20:58:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+
+This Code helps in converting speech to text
+
+
+For Execution Type : python speech_2_text.py
---
+++ speech_2_text.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+from gtts import tts
+import os
+import time
+import playsound
+
+r=sr.Recognizer()
+
+while True:
+    with sr.Microphone() as source:
+        print("Say Something... or Say Exit to Quit: ")
+        audio = r.listen(source)
+        text=r.recognize_google(audio)
+        print("You said:    "+text)
+        
+        if(text.lower() == 'exit'):
+            break
+
+print("Thank You")
